9befd8bb6be0189ad340daa24cf5b003b4288c17
KHJ <rlagudwns3555@gmail.com>
2025-02-10 01:51:25 +0900

    완료

--- PJT/js/index.js
+++ PJT/js/index.js
@@ -102,7 +102,7 @@ newData.forEach((x) => {
           <div class="host_info">
             <img
               class="host_photo"
-              src="https://a0.muscache.com/im/users/23360904/profile_pic/1415242605/original.jpg?im_w=240&im_format=avif"
+              src="${contextualPictures1}"
               alt=""
             />
             <div class="host_name">
